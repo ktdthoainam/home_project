70b5ed955f9ab3eaec8fefa5fc7a7b7c774e5cc9
ktdthoainam <129750448+ktdthoainam@users.noreply.github.com>
2023-04-14 20:44:30 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# home_project
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-74anun)
---
+++ script.js
@@ -0,0 +1,36 @@
+// console.log('Hello!');
+// console.log('Hello!');1
+// let date = prompt("hay nhap ngay");
+// let month = prompt("hay nhap thang");
+// let year = prompt("hay nhap nam");
+//   let message = `ngay: ${date} - ${month} - ${year}`
+// document.write(message);
+//bai tap 2
+// let money = prompt("Hay nhap so tien can vay (trieu):");
+// let percent = prompt("Lai suat vay cua ban la:");
+// let year = prompt("so nam ban vay la:");
+// const ONE_HUNDRED=100;
+// let total = Number(money) + Number((money * (percent/ONE_HUNDRED) * year));
+// let messenger = `<pre><h1> ung dung tinh lai suat ngan hang</h1>
+// so tien ban vay la ${money} trieu
+// so nam gui: ${year}
+// lai suat la: ${percent}%
+// so tien sau ${year} ban se co la ${total} </pre> `
+// document.write(messenger);
+
+
+//bai tap 3
+
+// let name = prompt("nhap ten nguoi nhan thu");
+// let address = prompt("nhap dia diem");
+// let time = prompt("Nhap thoi gian");
+// let message = ` <pre>${name} thương nhớ,
+
+// Em không biết phải nói sao để anh hiểu rằng, em nhớ anh thật nhiều. Em yêu anh đến khi trái tim này tan thành nghìn mảnh. Tất cả những gì em yêu thương, em khát khao và cần đến, chính là anh, mãi mãi về sau. Em chỉ muốn ở bên anh, và anh yêu hỡi, em sẽ trở thành người phụ nữ như anh mong muốn.
+
+// Có phải em thật tệ hại, khi cứ nghĩ đến anh thật nhiều, thật lâu và nhất là mỗi khi đêm xuống? Em hứa sẽ sẽ cố gắng triệu triệu lần hơn thế. Nhưng hơn tất cả, em chỉ mong một ngày nào đó, anh sẽ tự hào về em, như tự hào về vợ của anh, và mẹ của các con anh (ít nhất là 2 nhé, em vừa mới quyết định đấy!). Em nhớ thật nhiều cảm giác mỗi đêm anh ôm em và ru em ngủ trong vòng tay. Đêm nay, em chỉ muốn được gần bên anh… và anh biết không, trái tim em đang đau đớn đến nhường nào.
+
+// Anh yêu thương, đừng bao giờ rời xa em nữa nhé. Yêu anh rất nhiều.
+
+// ${address}, ${time}</prep>`
+// document.write(message);
